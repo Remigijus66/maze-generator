4b3f28ac5dd7b67795579c1347ccfd7ab3b6744a
Remigijus66 <remigijus.bartaska@gmail.com>
2023-05-25 17:06:41 +0300

    init

---
+++ main.js
@@ -0,0 +1,35 @@
+import Maze from "./maze.js";
+import { PATH_STR, readPathStr } from "./path.js";
+
+let maze;
+
+function resetMaze() {
+  maze = new Maze({
+    row_count: 10,
+    col_count: 10,
+    cell_size: 20,
+    // startCell: { row: ~~random(0, 5), col: ~~random(0, 5) },
+    // finishCell: { row: ~~random(15, 20), col: ~~random(15, 20) },
+  });
+  // maze.makePath(readPathStr(PATH_STR.C));
+  // maze.makePathFromText("ab", 1);
+  maze.generateMaze_DFS({
+    sleepTime: 1,
+  });
+}
+
+window.setup = () => {
+  createCanvas(200, 200);
+  resetMaze();
+};
+
+window.draw = () => {
+  background(50);
+  maze.draw();
+};
+
+window.keyPressed = () => {
+  if (keyCode == 13) {
+    resetMaze();
+  }
+};
